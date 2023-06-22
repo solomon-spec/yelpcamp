00084f402d15fc8759db0d1b36ce89ee06f17ee0
solomon abate <solomonabate18@gmail.com>
2023-06-22 07:07:50 -0700

    seed file,all camp, show one camp and new camp

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
-express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
-path = require('path');
+const campGround = require('./models/campground');
+const path = require('path');
+const methodOverride = require('method-override');
+
 app = express();
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/yelp-camp', 
+    {   useNewUrlParser: true, 
+        useUnifiedTopology: true
+     });
+
+mongoose.connection.once('open', function () {console.log('connected to mongodb')});
+mongoose.connection.on('error', function (error) {console.log('error connecting to mongodb')});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+// show all campgrounds
+app.get('/campgrounds', async function (req, res) {
+    const campgrounds = await campGround.find({});
+    res.render('campgrounds', {campgrounds});
+})
+
+// show one campground
+app.get('/campgrounds/show/:id', async function (req, res) {
+    const {id} = req.params;
+    const campground = await campGround.findById(id);
+    console.log(campground);
+    res.render('showcamp', {campground});
+});
+
+
+
+// new campgrounds
+
+app.get('/campgrounds/new', function (req, res) {
+    res.render('newcamp');
+})
+app.post('/campgrounds/new', async function (req, res) {
+
+    const newcamp = new campGround({title:req.body.title, location: req.body.location});
+    newcamp.save();
+    res.redirect('/campgrounds');
+})
 
 
 
