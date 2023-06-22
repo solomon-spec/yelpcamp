67180cd9a5e4d83a1b3a04a75fff874ed8dfd20c
solomon abate <solomonabate18@gmail.com>
2023-06-22 02:40:48 -0700

    git

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 express = require('express');
+path = require('path');
 app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
