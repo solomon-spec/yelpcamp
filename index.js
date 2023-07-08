8e13e90fe49b1a15ae6f953b10d8509189573ba4
solomon abate <solomonabate18@gmail.com>
2023-07-07 22:56:20 -0700

    mode restructuring

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const campGround = require('./models/campground');
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const asyncwrap = require('./utils/asyncwrap');
 const ExpressError = require('./utils/expressError');
-const { validateCampground, validateReview } = require('./utils/validatecampgroung');
-const Review = require('./models/review');
+
 const campgroundRouter = require('./router/campgroundrouter');
 const reviewRouter = require('./router/reviewrouter');
 
@@ -15,7 +12,9 @@ app = express();
 mongoose.connect('mongodb://localhost:27017/yelp-camp',
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
     });
 
 mongoose.connection.once('open', function () { console.log('connected to mongodb') });
@@ -23,10 +22,12 @@ mongoose.connection.on('error', function (error) { console.log('error connecting
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('pages/home');
@@ -38,12 +39,15 @@ app.use('/campgrounds/:id/review', reviewRouter);
 //campgrounds router
 app.use('/campgrounds', campgroundRouter);
 
-
+app.all('*', function (req, res, next) {
+    err = new ExpressError(404, 'page not found');
+    next(err);
+});
 
 // error handling
 app.use(function (err, req, res, next) {
     let { message = 'something went wrong', statusCode = 500 } = err;
-    res.status(statusCode).send(message + err);
+    res.status(statusCode).send(message);
 });
 
 app.listen(3000, function () {
